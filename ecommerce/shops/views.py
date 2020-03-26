27d151f048ee29c6f3826a38afe416b2d5385fe4
kisumutalent <homiemusa@gmail.com>
2020-03-26 08:52:12 +0300

    Complate add to cart

--- ecommerce/shops/views.py
+++ ecommerce/shops/views.py
@@ -1,8 +1,11 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.shortcuts import render, get_object_or_404
+from django.shortcuts import redirect
 from django.views.generic import ListView, DetailView
+from django.utils import timezone
 # Create your views here.
 
-from .models import Item
+from .models import Item, OrderItem, Order
 
 def products(request):
 	context = {
@@ -26,3 +29,56 @@ class ItemDetailView(DetailView):
 	model = Item
 	template_name = "product.html"
 
+def add_to_cart(request, slug):
+	item = get_object_or_404(Item, slug=slug)
+	order_item, created = OrderItem.objects.get_or_create(
+		item=item,
+		user=request.user,
+		ordered=False
+	)
+	order_qs = Order.objects.filter(user=request.user, ordered=False)
+	if order_qs.exists():
+		order = order_qs[0]
+
+		# check if the order item is in the order
+		if order.items.filter(item__slug=item.slug).exists():
+			order_item.quantity += 1
+			order_item.save()
+			messages.info(request, "This item quantity was updated")
+		else:
+			messages.info(request, "This item was added to your cart.")
+			order.items.add(order_item)
+	else:
+		ordered_date = timezone.now()
+		order = Order.objects.create(
+			user=request.user, ordered_date=ordered_date)
+		order.items.add(order_item)
+		messages.info(request, "This item was added to your cart")
+	return redirect("shops:product", slug=slug)
+
+
+def remove_from_cart(request, slug):
+	item = get_object_or_404(Item, slug=slug)
+	# Check if user has orders
+	order_qs = Order.objects.filter(
+		user=request.user,
+		ordered=False
+	)
+	if order_qs.exists():
+		order = order_qs[0]
+
+		if order.items.filter(item__slug=item.slug).exists():
+			order_item = OrderItem.objects.filter(
+				item=item,
+				user=request.user,
+				ordered=False
+			)[0]
+			order.items.remove(order_item)
+			messages.info(request, "This Item was removed from cart")
+			return redirect("shops:product", slug=slug)
+		else:
+			messages.info(request, "This was not in Cart")
+			return redirect("shops:product", slug=slug)			
+	else:
+		messages.info(request, "You do not have an active order")
+		return redirect("shops:product", slug=slug)
